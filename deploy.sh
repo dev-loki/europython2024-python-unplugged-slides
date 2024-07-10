9806de534490213550eb199b920a715c16293737
Torsten Zielke <t.zielke@mara.solutions>
2024-07-10 20:09:14 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,9 @@
 source ".env"
 
 REMOTE="$ZOLA_USER@$ZOLA_DOMAIN:$ZOLA_REMOTE_FOLDER/slides/vortraege/ep2024-python-unplugged"
-pnpm run build && scp -i "$ZOLA_KEY" -r dist/* "$REMOTE"
+LOCAL="$HOME/project/lokidev-blog/static/slides/vortraege/ep2024-python-unplugged"
+
+pnpm run build --base /slides/vortraege/ep2024-python-unplugged/
+scp -i "$ZOLA_KEY" -r dist/* "$REMOTE"
+cp -r dist "$LOCAL"
 
