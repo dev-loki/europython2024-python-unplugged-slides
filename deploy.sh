197e5a11217eb4ea7027b852f86c4b01de19b433
Torsten Zielke <t.zielke@mara.solutions>
2024-07-11 09:32:41 +0200

    Also move slides to my blog project

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 source ".env"
 
-REMOTE="$ZOLA_USER@$ZOLA_DOMAIN:$ZOLA_REMOTE_FOLDER/slides/vortraege/ep2024-python-unplugged"
-LOCAL="$HOME/project/lokidev-blog/static/slides/vortraege/ep2024-python-unplugged"
-
 pnpm run build --base /slides/vortraege/ep2024-python-unplugged/
-scp -i "$ZOLA_KEY" -r dist/* "$REMOTE"
+pnpm run export
+
+# REMOTE="$ZOLA_USER@$ZOLA_DOMAIN:$ZOLA_REMOTE_FOLDER/slides/vortraege/ep2024-python-unplugged"
+# scp -i "$ZOLA_KEY" -r dist/* "$REMOTE"
+
+LOCAL="$HOME/project/lokidev-blog/static/slides/vortraege/ep2024-python-unplugged"
 cp -r dist "$LOCAL"
 
+mv slides-export.pdf "$LOCAL/europython2024-python-unplugged-slides.pdf"
